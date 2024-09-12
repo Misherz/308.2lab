997b506eb1d2282a86c815f71cfefb27fe5764f7
Misherz <michelle.j.lugo@gmail.com>
2024-09-11 21:03:55 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const PI = 3.1415; //given info
+const radius = 5; //given info, assigning variable radius to 5
+const area = PI * radius * radius; //given info
+
+let spacePerPlant = 0.08; //diameter per plant
+
+
+//plants double per week
+//Statements for outputs
+//area
+
+//Predict the plant growth after a specific number of weeks. (3)
+        //calculating max plant to for area
+
+console.log("The area of the container is " + area)
+
+maxPlant = ''
+let plants = 20;
+let x = area/spacePerPlant;
+let y = spacePerPlant/plants;
+
+if (maxPlant = true){
+    console.log(`At least ${x} plants fit in this area.`)
+} if(maxPlant = true){
+    let maxPlant = x / y;
+    console.log(`Max amount of plants is: ` + maxPlant);
+} if (maxPlant = true){
+    for (let plants=20; plants <= maxPlant; plants * 2) {
+    let weeks = 1;
+    console.log(`Week ${weeks} should have: ${plants}.`);
+    weeks++;
+}
+}
+
